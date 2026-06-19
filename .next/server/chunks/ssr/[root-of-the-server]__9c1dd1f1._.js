6e1d27f9298ef17f4d6c5dc05058a6911fd06282
Arjun Devendra <arjun@growve.in>
2026-06-19 10:55:12 +0530

    Remove .next folder from repository

--- .next/server/chunks/ssr/[root-of-the-server]__9c1dd1f1._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[93695,(a,b,c)=>{b.exports=a.x("next/dist/shared/lib/no-fallback-error.external.js",()=>require("next/dist/shared/lib/no-fallback-error.external.js"))},18268,(a,b,c)=>{}];
-
-//# sourceMappingURL=%5Broot-of-the-server%5D__9c1dd1f1._.js.map
--- .next/static/chunks/src_app_page_tsx_949a52ff._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_1b39c842._.js",
-  "static/chunks/node_modules_bf6f1a31._.js"
-],
-    source: "dynamic"
-});
