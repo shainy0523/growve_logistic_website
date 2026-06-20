154fe6f9030745cbb60331277d3d1b1925740b46
Arjun Devendra <arjun@growve.in>
2026-06-20 17:44:41 +0530

    feat: Implemented Contact Us Page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import LandingLayout from '@/layouts/LandingLayout'
+import ContactView from '@/views/contact/ContactView'
+
+export default function ContactPage() {
+  return (
+    <LandingLayout>
+      <ContactView />
+    </LandingLayout>
+  )
+}
